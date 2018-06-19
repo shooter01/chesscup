0f519e80cd613ced19327ab21d55323cca5bfa8f
aidar <shooter@mail.ru>
2018-06-19 23:38:28 +0300

    second

--- app.js
+++ app.js
@@ -1,26 +1,133 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var MySQLStore = require('express-mysql-session')(session);
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var flash = require('connect-flash');
+var Database = require('./routes/sql');
+
+
 
 var app = express();
+var engine = require('ejs-mate');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs', engine);
 
-app.use(favicon());
+var pool = new Database({
+    host: 'localhost',
+    user: 'root',
+    password: 'ubuntu34',
+    database: 'chess',
+    connectionLimit: 10,
+    acquireTimeout: 1000
+});
+
+var sessionStore = new MySQLStore({}, pool.getConnection());
+
+
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(flash());
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+    res.locals.user = req.session.passport;
+    next();
+});
+
+
+var getUser = function(email, password, cb) {
+    pool.query("SELECT * FROM users WHERE email = ? AND password = ? LIMIT 1", [email, password])
+        .then(function (rows) {
+            if (rows.length > 0) {
+                cb(null, rows[0]);
+            } else {
+                return cb(null, null);
+            }
+        }).catch(function (err) {
+        console.log(err);
+    });
+};
+
+
+passport.serializeUser(function(user, done) {
+    let is_sbornik = false;
+    if (user.email === "nostsaber@yandex.ru") {
+        is_sbornik = true;
+    }
+    console.log(user);
+    done(null, {
+        id : user.id,
+        name : user.name,
+        role : user.role,
+        rating : user.rating,
+        is_paid : user.is_paid,
+        status : statuses[user.role].title,
+        is_sbornik : is_sbornik,
+        school_id : user.school_id
+    });
+});
+
+passport.deserializeUser(function(user, done) {
+    pool.query("SELECT * FROM users WHERE id = ? LIMIT 1", user.id)
+        .then(function (rows) {
+            done(null, user.id);
+        }).catch(function (err) {
+        console.log(err);
+    });
+});
+
+passport.use("local-login",new LocalStrategy({
+        usernameField: 'username',
+        passwordField: 'password',
+        passReqToCallBack: true
+    },
+    function(username, password, done) {
+        // return done(null, false, {message: 'Pas d\'utilisateur avec ce login.'});
+        getUser(username, password, function(err, user) {
+            if (err)
+                return done(err);
+            if (!user)
+                return done(null, false, {message: 'Введенные данные некорректны. Проверьте логин и пароль.'});
+            /*if (!user.validPassword(password))
+             return done(null, false, {message: 'Oops! Mauvais password.'});*/
+
+
+            return done(null, user);
+        });
+
+    }
+));
+
+
+var routes = require('./routes/index')(app, passport, pool);
+var users = require('./routes/users');
 app.use('/', routes);
 app.use('/users', users);
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,47 @@
 var express = require('express');
+// var mysql = require('mysql-promise');
 var router = express.Router();
+const moment = require('moment');
+const { check, validationResult } = require('express-validator/check');
+const { matchedData, sanitize } = require('express-validator/filter');
+const countries = require('./countries');
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+
+var Elo = require('arpad');
+
+var uscf = {
+    default: 20,
+    2100: 15,
+    2400: 10
+};
+
+var min_score = 100;
+var max_score = 10000;
+
+var elo = new Elo(uscf, min_score, max_score);
+
+
+module.exports = function (app, passport, pool) {
+  /* GET home page. */
+    router.get('/', function(req, res) {
+
+
+        pool.query('SELECT tournaments.*, users.name FROM tournaments LEFT JOIN users on users.id = tournaments.school_id ORDER BY tournaments.id DESC').then(function (results) {
+
+            res.render('index', {
+                tournaments : results,
+                moment: moment,
+                title: 'Tournaments',
+                countries : countries
+
+            });
+
+        }).catch(function (err) {
+            console.log(err);
+        });
+
+    });
+
+    return router;
+};
