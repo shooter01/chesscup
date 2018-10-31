06da30f974fc1435e7ffa26e7797cb3f4eb833a8
aidar <shooter@mail.ru>
2018-10-31 23:38:20 +0300

    before refactoring hte game

--- routes/index.js
+++ routes/index.js
@@ -45,16 +45,13 @@ module.exports = function (app, passport, pool) {
         function (req, res, next) {
             let chat_id = req.params.chat_id;
             var tourney, participants, is_in = false;
-            console.log(chat_id);
             if (chat_id) {
 
                 app.mongoDB.collection("chat").find({chat_id: chat_id}, function(err, cursor) {
                     let messages = [];
                     cursor.forEach(function (message) {
                         messages.push(message);
-
                     }, function () {
-                        console.log(messages);
                         res.json({
                             status : "ok",
                             messages : JSON.stringify(messages)
